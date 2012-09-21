4b9ac140a32679ad6e0671f300c830814bad2e7a
Xavier Ducrohet <xav@android.com>
2012-09-20 19:36:33 -0700

    Only create R class for libraries when the project is not a library.
    Change-Id: Ia3359fd389aee6fc15796e1e6834cdd4864ecb09

--- builder/src/main/java/com/android/builder/AndroidBuilder.java
+++ builder/src/main/java/com/android/builder/AndroidBuilder.java
@@ -596,12 +596,13 @@ public class AndroidBuilder {
         // library specific options
         if (mVariant.getType() == VariantConfiguration.Type.LIBRARY) {
             command.add("--non-constant-id");
-        }
-
-        String extraPackages = mVariant.getLibraryPackages();
-        if (extraPackages != null) {
-            command.add("--extra-packages");
-            command.add(extraPackages);
+        } else {
+            // only create the R class from library dependencies if this is not a library itself.
+            String extraPackages = mVariant.getLibraryPackages();
+            if (extraPackages != null) {
+                command.add("--extra-packages");
+                command.add(extraPackages);
+            }
         }
 
         // AAPT options
