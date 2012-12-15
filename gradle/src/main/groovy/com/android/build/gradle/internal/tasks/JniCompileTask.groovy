f43540271d7dc419bf056d5c090a433863b539eb
Dmitry Polishuk <dp@neosphere.com>
2012-12-15 13:57:18 +0400

    working on jni support

---
+++ gradle/src/main/groovy/com/android/build/gradle/internal/tasks/JniCompileTask.groovy
@@ -0,0 +1,12 @@
+package com.android.build.gradle.internal.tasks
+
+import com.android.build.gradle.tasks.JniCompile
+
+/**
+ * User: Dmitry Polishuk <dmitry.polishuk@gmail.com>
+ * Date: 12/10/12
+ * Time: 12:15 AM
+ */
+class JniCompileTask extends JniCompile {
+
+}
